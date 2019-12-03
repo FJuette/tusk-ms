1d75b9fa7e5cb7b21c0e1ce2e62d698a80fff780
Fabian Juette <Fabian.Juette@tu-clausthal.de>
2019-12-03 15:53:17 +0100

    removed ionide

--- clone.sh
+++ clone.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-OLDNAME='Tusk'
-NEWNAME='Magnus'
-rm -Rf ./../$NEWNAME
-cp -rf ./../tusk-ms ./../$NEWNAME
-cd ./../$NEWNAME
-rm -Rf .git
-rm -Rf .vs
-rm -Rf ./src/$OLDNAME.Story/obj
-rm -Rf ./src/$OLDNAME.Story/bin
-rm -Rf ./tests/$OLDNAME.Story.Tests/obj
-rm -Rf ./tests/$OLDNAME.Story.Tests/bin
-find . -type d -name '*'$OLDNAME'*' -print0 | xargs -0 -n1 bash -c 'mv "$0" "${0/'$OLDNAME'/'$NEWNAME}'"';
-find . -type f -name '*'$OLDNAME'*' -print0 | xargs -0 -n1 bash -c 'mv "$0" "${0/'$OLDNAME'/'$NEWNAME}'"'
-find . -type f -name '*' -exec sed -i "" 's/'$OLDNAME'/'$NEWNAME'/g' {} +
+#Replace Magnus.Api with desired name and the following foldername
+FOLDERNAME='Magnus'
+rm -Rf ./../$FOLDERNAME
+cp -rf ./../tusk-ms ./../$FOLDERNAME
+cd ./../$FOLDERNAME
+pwd
+rm -Rf ./.git
+rm -Rf ./.vs
+rm -Rf ./src/Tusk.Story/obj
+rm -Rf ./src/Tusk.Story/bin
+rm -Rf ./tests/Tusk.Story.Tests/obj
+rm -Rf ./tests/Tusk.Story.Tests/bin
+mv Tusk.sln Magnus.sln
+find . -type d -name '*Tusk.Story*' -print0 | xargs -0 -n1 bash -c 'mv "$0" "${0/Tusk.Story/Magnus.Api}"'
+find . -type f -name '*Tusk.Story*' -print0 | xargs -0 -n1 bash -c 'mv "$0" "${0/Tusk.Story/Magnus.Api}"'
+find . -type f -name '*' -exec sed -i "" 's/Tusk\.Story/Magnus\.Api/g' {} +
